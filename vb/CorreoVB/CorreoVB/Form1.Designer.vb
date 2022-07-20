a71199619aff101fc6e1d9cb11e31a49a8d6631e
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 11:45:44 -0600

    Add files via upload

---
+++ vb/CorreoVB/CorreoVB/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCorreoVB
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.btnValidar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(312, 101)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(179, 20)
+        Me.txtCorreo.TabIndex = 0
+        '
+        'btnValidar
+        '
+        Me.btnValidar.Location = New System.Drawing.Point(354, 137)
+        Me.btnValidar.Name = "btnValidar"
+        Me.btnValidar.Size = New System.Drawing.Size(72, 35)
+        Me.btnValidar.TabIndex = 1
+        Me.btnValidar.Text = "Validar"
+        Me.btnValidar.UseVisualStyleBackColor = True
+        '
+        'frmCorreoVB
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Red
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnValidar)
+        Me.Controls.Add(Me.txtCorreo)
+        Me.Name = "frmCorreoVB"
+        Me.Text = "CorreoVB"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents btnValidar As Button
+End Class
---
+++ vb/CorreoVB/CorreoVB/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class frmCorreoVB
+
+    Dim arroba As Boolean
+
+    Private Sub btnValidar_Click(sender As Object, e As EventArgs) Handles btnValidar.Click
+
+        If (((txtCorreo.Text Like "[a-z]*@[!.]?*.[a-z][a-z][a-z].[a-z][a-z]") Or (txtCorreo.Text Like "[a-z]*@[!.]?*.[a-z][a-z][a-z]")) And Not (txtCorreo.Text Like "*@*@*") And Not (txtCorreo.Text Like "*@*.*.*.*") And Not (txtCorreo.Text Like "*--*") And Not (txtCorreo.Text Like "*..*")) Then
+
+            If ((txtCorreo.Text Like "*[/]*") Or (txtCorreo.Text Like "*[!]*") Or (txtCorreo.Text Like "*[*]*") Or (txtCorreo.Text Like "*[,]*") Or (txtCorreo.Text Like "*[[]*") Or (txtCorreo.Text Like "*[]]*") Or (txtCorreo.Text Like "*[\]*") Or (txtCorreo.Text Like "*[+]*") Or (txtCorreo.Text Like "*[=]*") Or (txtCorreo.Text Like "*[{]*") Or (txtCorreo.Text Like "*[}]*") Or (txtCorreo.Text Like "*[|]*") Or (txtCorreo.Text Like "*[&]*") Or (txtCorreo.Text Like "*[(]*") Or (txtCorreo.Text Like "*[)]*") Or (txtCorreo.Text Like "*[;]*") Or (txtCorreo.Text Like "*[:]*") Or (txtCorreo.Text Like "*[']*") Or (txtCorreo.Text Like "*[^]*") Or (txtCorreo.Text Like "*[%]*") Or (txtCorreo.Text Like "*[$]*") Or (txtCorreo.Text Like "*[#]*") Or (txtCorreo.Text Like "*[`]*") Or (txtCorreo.Text Like "*[~]*") Or (txtCorreo.Text Like "*[<]*") Or (txtCorreo.Text Like "*[>]*") Or (txtCorreo.Text Like "*[ ]*") Or (txtCorreo.Text Like "*[?]*")) Then
+
+                MsgBox("Correo Invalido")
+
+            Else
+
+                MsgBox("Correo Valido")
+
+            End If
+
+
+
+        Else
+
+            MsgBox("Correo Invalido")
+
+        End If
+
+
+    End Sub
+
+
+
+End Class

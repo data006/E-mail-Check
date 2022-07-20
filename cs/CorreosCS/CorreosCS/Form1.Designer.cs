79ba4048a67d32618037f76a41bd9a3f08acde45
data006 <109679683+data006@users.noreply.github.com>
2022-07-20 11:58:11 -0600

    Add files via upload

---
+++ cs/CorreosCS/CorreosCS/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CorreosCS
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnValidar = new System.Windows.Forms.Button();
+			this.txtCorreo = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnValidar
+			// 
+			this.btnValidar.Location = new System.Drawing.Point(96, 68);
+			this.btnValidar.Name = "btnValidar";
+			this.btnValidar.Size = new System.Drawing.Size(72, 35);
+			this.btnValidar.TabIndex = 3;
+			this.btnValidar.Text = "Validar";
+			this.btnValidar.UseVisualStyleBackColor = true;
+			this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+			// 
+			// txtCorreo
+			// 
+			this.txtCorreo.Location = new System.Drawing.Point(54, 32);
+			this.txtCorreo.Name = "txtCorreo";
+			this.txtCorreo.Size = new System.Drawing.Size(179, 20);
+			this.txtCorreo.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Lime;
+			this.ClientSize = new System.Drawing.Size(283, 133);
+			this.Controls.Add(this.btnValidar);
+			this.Controls.Add(this.txtCorreo);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		internal System.Windows.Forms.Button btnValidar;
+		internal System.Windows.Forms.TextBox txtCorreo;
+	}
+}
+
---
+++ cs/CorreosCS/CorreosCS/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace CorreosCS
+{
+
+	
+
+
+
+
+
+
+
+
+
+	public partial class Form1 : Form
+	{
+
+		public bool arroba;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		private void btnValidar_Click(object sender, EventArgs e)
+		{
+
+			if ((LikeOperator.LikeString(txtCorreo.Text, "[a-z]*@[!.]?*.[a-z][a-z][a-z].[a-z][a-z]", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "[a-z]*@[!.]?*.[a-z][a-z][a-z]", CompareMethod.Binary)) & !LikeOperator.LikeString(txtCorreo.Text, "*@*@*", CompareMethod.Binary) & !LikeOperator.LikeString(txtCorreo.Text, "*@*.*.*.*", CompareMethod.Binary) & !LikeOperator.LikeString(txtCorreo.Text, "*--*", CompareMethod.Binary) & !LikeOperator.LikeString(txtCorreo.Text, "*..*", CompareMethod.Binary))
+			{
+				if (LikeOperator.LikeString(txtCorreo.Text, "*[/]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[!]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[*]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[,]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[[]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[]]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, @"*[\]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[+]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[=]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[{]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[}]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[|]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[&]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[(]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[)]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[;]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[:]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[']*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[^]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[%]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[$]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[#]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[`]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[~]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[<]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[>]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[ ]*", CompareMethod.Binary) | LikeOperator.LikeString(txtCorreo.Text, "*[?]*", CompareMethod.Binary))
+				{
+					MessageBox.Show("Correo Invalido");
+				}
+				else
+				{
+					MessageBox.Show("Correo Valido");
+				}
+			}
+			else
+			{
+				MessageBox.Show("Correo Invalido");
+			}
+
+		}
+
+
+
+
+
+	}
+}
